6ed6cdef47343c7bb21b6aa2ac438890413fc686
Sujin <sujin.v2px@gmail.com>
2019-07-28 16:10:54 +0545

    create many to many relationship with hospital and pharmacy

--- src/api/models/pharmacy.model.js
+++ src/api/models/pharmacy.model.js
@@ -1,4 +1,5 @@
 import Model from '../../database/db';
+import { Hospital } from './';
 
 const TABLE_NAME = 'pharmacy';
 
@@ -9,5 +10,22 @@ export default class Pharmacy extends Model {
 
   static get idColumn() {
     return 'id';
+  }
+
+  static get relationMappings() {
+    return {
+      hospitals: {
+        relation: Model.ManyToManyRelation,
+        modelClass: Hospital,
+        join: {
+          from: 'pharmacy.id',
+          through: {
+            from: 'phar_hosp.pharmacy_id',
+            to: 'phar_hosp.hospital_id'
+          },
+          to: 'hospitals.id'
+        }
+      }
+    };
   }
 }
