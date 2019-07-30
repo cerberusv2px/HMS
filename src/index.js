b68a780a9258c29b808ed1012cb61c8924c69577
Sujin <sujin.v2px@gmail.com>
2019-07-30 15:26:58 +0545

    added favicon and added cors middlewares

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import './env';
 // import fs from 'fs';
 import express from 'express';
 import helmet from 'helmet';
+import cors from 'cors';
+import path from 'path';
 import bodyParser from 'body-parser';
 import compression from 'compression';
 import morgan from 'morgan';
+import favicon from 'serve-favicon';
 import json from './middlewares/json';
 import logger, { logStream } from './utils/logger';
 import * as errorHandler from './middlewares/errorHandler';
@@ -20,6 +23,9 @@ const APP_HOST = process.env.APP_HOST || '0.0.0.0';
 app.set('port', APP_PORT);
 app.set('host', APP_HOST);
 
+app.use(favicon(path.join(__dirname, '/../public', 'favicon.ico')));
+
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,7 +40,7 @@ app.use(errorHandler.genericErrorHandler);
 app.use(errorHandler.methodNotAllowed);
 
 app.listen(app.get('port'), app.get('host'), () => {
-  logger.info(`Server started at https://${app.get('host')}:${app.get('port')}/api`);
+  logger.info(`Server started at http://${app.get('host')}:${app.get('port')}/api`);
 });
 
 export default app;
