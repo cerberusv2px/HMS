87c2799ea0cf13850e63e9a2773635a9b5b48141
Sujin <sujin.v2px@gmail.com>
2019-08-06 14:32:15 +0545

    added async await in controller

--- src/api/controllers/doctor.controller.js
+++ src/api/controllers/doctor.controller.js
@@ -3,11 +3,13 @@ import { doctorService } from '../services';
 
 let router = Router();
 
-router.get('/', (req, res, next) => {
-  doctorService
-    .fetchAll()
-    .then(data => res.json({ data }))
-    .catch(err => next(err));
+router.get('/', async (req, res, next) => {
+  try {
+    const doctor = await doctorService.fetchAll();
+    res.json({ data: doctor });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export default router;
